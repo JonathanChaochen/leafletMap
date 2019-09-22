42643d815804a3e4f8518fab246f2239b1f6b7dd
Jonathan Chen <chaochen42@gmail.com>
2019-09-23 11:28:44 +1200

    update offset according t omapbox  docs
    https://docs.mapbox.com/mapbox.js/example/v1.0.0/leaflet-studio-style/

--- src/components/BaseLayerControl.js
+++ src/components/BaseLayerControl.js
@@ -26,6 +26,7 @@ const BaseLayerControl = () => (
               "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
             }
             url={`https://api.mapbox.com/styles/v1/mapbox/${styleName}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hhb2NoZW4iLCJhIjoiY2p3c2xqYW1pMDNxZzRibHlodjFuMmQwbiJ9.ntibxY-is20Rz4GMgA1Jww`}
+            zoomOffset={-1} 
           />
         </LayersControl.BaseLayer>
       );
