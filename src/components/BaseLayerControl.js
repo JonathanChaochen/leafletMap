bd5de3fcc33116ba1f2a5de897942a9bd523e262
Jonathan Chen <chaochen42@gmail.com>
2019-09-23 13:31:04 +1200

    detectRetina

--- src/components/BaseLayerControl.js
+++ src/components/BaseLayerControl.js
@@ -22,6 +22,7 @@ const BaseLayerControl = () => (
       return (
         <LayersControl.BaseLayer checked={index === 0} name={styleName}>
           <TileLayer
+            detectRetina
             attribution={
               "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
             }
